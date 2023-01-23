d7177b7020bd013858a68a60592cda2555ee4f82
XE31 <axel.ibanez2@lycee-pardailhan.fr>
2023-01-23 15:42:55 +0100

    test

---
+++ php/artiste.php
@@ -0,0 +1,25 @@
+<?php
+// database connection code
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+
+$con = mysqli_connect('localhost', 'root', '','cagroove');
+
+// get the post records
+$txtName = $_POST['txtName'];
+$style = $_POST['style'];
+$horaire = $_POST['horaire'];
+$reseaux = $_POST['reseaux'];
+$nationaliter = $_POST['nationaliter']
+
+// database insert SQL code
+$sql = "INSERT INTO `artiste` ( `fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ( '$txtName', '$style', '$horaire', '$reseaux', '$nationaliter')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+
+if($rs)
+{
+	echo "Contact Records Inserted";
+}
+
+?>
