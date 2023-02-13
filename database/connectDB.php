24eefdb5a30f577baf5c45d82d67be9cd91026f0
Guillaume Degan <guillaume.degan@free.fr>
2023-02-13 10:48:26 +0100

    test

--- database/connectDB.php
+++ database/connectDB.php
@@ -8,7 +8,7 @@ class ConnectDB{
     private $db_host;
     private $pdo;
 
-    public function __construct($db_name, $db_user = 'root', $db_pass='root', $db_host='localhost') {
+    public function __construct($db_name, $db_user = 'root', $db_pass='', $db_host='localhost') {
         $this->db_name = $db_name;
         $this->db_user = $db_user;
         $this->db_pass = $db_pass;
@@ -17,7 +17,7 @@ class ConnectDB{
 
     public function getPDO() {
         if($this->pdo === null) {
-            $pdo = new PDO('mysql:dbname=cagroove;host=localhost','root','');
+            $pdo = new PDO("mysql:dbname=$this->db_name;host=$this->db_host",$this->db_user,$this->db_pass);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo = $pdo;
         }
--- utilisateur/affichage_utilisateur.php
+++ utilisateur/affichage_utilisateur.php
@@ -7,32 +7,10 @@
     <title>Document</title>
 
     <?php
-// Connect to the database
-// $con = mysqli_connect('localhost', 'root', '','cagroove');
-
-// // Check connection
-// if (!$con) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-
-// // Select all data from the table
-// $sql = "SELECT * FROM utilisateur";
-// $result = mysqli_query($con, $sql);
-
-// // Fetch the data into an array
-// $data = array();
-// while ($row = mysqli_fetch_assoc($result)) {
-//     $data[] = $row;
-// }
-
-// // Close the connection
-// mysqli_close($con);
-
 require "../database/connectDB.php";
 $db = new ConnectDB('cagroove');
 $data = $db->query('SELECT * FROM utilisateur');
 $array = json_decode(json_encode($data), true);
-var_dump($array);
 ?>
 </head>
 <body>
