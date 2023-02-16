17a69a77c54f1411f7428dfcf696605e6000c9c0
Guillaume Degan <guillaume.degan@free.fr>
2023-02-16 09:41:22 +0100

    gestion des suppressions avec les tables relationnelles des passions et gouts

--- utilisateur/supprimer_utilisateur.php
+++ utilisateur/supprimer_utilisateur.php
@@ -8,10 +8,6 @@ $db = new ConnectDB('cagroove');
 // récupération de l'id user à supprimer
 $id = $_GET['id'];
 
-// suppréssion des champs liés à l'user dans la table utilisateursgouts et utilisateurspassions
-$db->querySend("DELETE FROM utilisateursgouts WHERE `idUtilisateur` = $id ;");
-$db->querySend("DELETE FROM utilisateurspassions WHERE `idUtilisateur` = $id ;");
-
 // suppréssion de l'user en question
 $db->querySend("DELETE FROM utilisateur WHERE `utilisateur`.`id` = $id ;");
 
